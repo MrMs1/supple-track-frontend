a321a226cf2946ed3ba32cd491986da6ce05fc06
MsMr1 <johnuinchester@gmail.com>
2025-01-04 15:38:57 +0900

    refactor: suspenseを削除

--- src/app/_containers/supplement_list/presentational.tsx
+++ src/app/_containers/supplement_list/presentational.tsx
@@ -1,52 +1,18 @@
 "use client";
 
 import type { Supplement } from "@/app/_types/types";
-import { Skeleton } from "@nextui-org/skeleton";
 import { Pill } from "lucide-react";
 import React, { Suspense, useOptimistic, useTransition } from "react";
 import { CreateSupplementButton } from "./_components/CreateSupplementButton";
 import { EmptySupplementCard } from "./_components/EmptySupplementCard";
 import { deleteSupplementAction } from "./_components/deleteSupplementAction";
 import { SupplementCard } from "./_components/supplement/SupplementCard";
-function SupplementListSkeleton() {
-  return (
-    <div className="space-y-4">
-      <div className="flex items-center justify-between mb-6">
-        <div className="flex items-center">
-          <Pill className="h-6 w-6 text-indigo-600 mr-2" />
-          <h2 className="text-2xl font-bold text-gray-900">サプリメント一覧</h2>
-        </div>
-      </div>
-      <div className="space-y-4">
-        {[1, 2, 3].map((i) => (
-          <div key={i} className="bg-white border rounded-lg p-4">
-            <div className="flex justify-between items-center">
-              <div className="flex-grow">
-                <Skeleton className="rounded-lg">
-                  <div className="h-7 w-32 rounded-lg bg-default-200" />
-                </Skeleton>
-                <div className="mt-2 grid grid-cols-2 gap-4 w-[400px]">
-                  <Skeleton className="rounded-lg">
-                    <div className="h-5 w-full rounded-lg bg-default-200" />
-                  </Skeleton>
-                  <Skeleton className="rounded-lg">
-                    <div className="h-5 w-full rounded-lg bg-default-200" />
-                  </Skeleton>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+interface SupplementsProps {
+  supplements: Supplement[] | null;
 }
 
-function SupplementsPresentation({
-  supplements,
-}: {
-  supplements: Supplement[] | null;
-}) {
+export function SupplementsPresentation({ supplements }: SupplementsProps) {
   const [_, startTransition] = useTransition();
   const [optimisticSupplements, setOptimisticSupplements] = useOptimistic(
     supplements,
@@ -97,19 +63,5 @@ function SupplementsPresentation({
         </div>
       </div>
     </>
-  );
-}
-
-interface SupplementsProps {
-  supplements: Supplement[] | null;
-}
-
-export default function SupplementsWithSuspense({
-  supplements,
-}: SupplementsProps) {
-  return (
-    <Suspense fallback={<SupplementListSkeleton />}>
-      <SupplementsPresentation supplements={supplements} />
-    </Suspense>
   );
 }
