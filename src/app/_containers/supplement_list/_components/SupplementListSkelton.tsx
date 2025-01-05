555483e59b61cc1debe4546c5701a0e10c7d96b2
MsMr1 <johnuinchester@gmail.com>
2025-01-05 15:00:47 +0900

    feat: リトライ処理をasync-retryで行うように
    スケルトンのスタイルも修正

--- src/app/_containers/supplement_list/_components/SupplementListSkelton.tsx
+++ src/app/_containers/supplement_list/_components/SupplementListSkelton.tsx
@@ -3,33 +3,37 @@ import { Pill } from "lucide-react";
 
 export function SupplementListSkeleton() {
   return (
-    <div className="space-y-4">
-      <div className="flex items-center justify-between mb-6">
-        <div className="flex items-center">
-          <Pill className="h-6 w-6 text-indigo-600 mr-2" />
-          <h2 className="text-2xl font-bold text-gray-900">サプリメント一覧</h2>
-        </div>
-      </div>
+    <div className="bg-white rounded-lg shadow-xl p-6">
       <div className="space-y-4">
-        {[1, 2, 3].map((i) => (
-          <div key={i} className="bg-white border rounded-lg p-4">
-            <div className="flex justify-between items-center">
-              <div className="flex-grow">
-                <Skeleton className="rounded-lg">
-                  <div className="h-7 w-32 rounded-lg bg-default-200" />
-                </Skeleton>
-                <div className="mt-2 grid grid-cols-2 gap-4 w-[400px]">
+        <div className="flex items-center justify-between mb-6">
+          <div className="flex items-center">
+            <Pill className="h-6 w-6 text-indigo-600 mr-2" />
+            <h2 className="text-2xl font-bold text-gray-900">
+              サプリメント一覧
+            </h2>
+          </div>
+        </div>
+        <div className="space-y-4">
+          {[1, 2, 3].map((i) => (
+            <div key={i} className="bg-white border rounded-lg p-4">
+              <div className="flex justify-between items-center">
+                <div className="flex-grow">
                   <Skeleton className="rounded-lg">
-                    <div className="h-5 w-full rounded-lg bg-default-200" />
+                    <div className="h-7 w-32 rounded-lg bg-default-200" />
                   </Skeleton>
-                  <Skeleton className="rounded-lg">
-                    <div className="h-5 w-full rounded-lg bg-default-200" />
-                  </Skeleton>
+                  <div className="mt-2 grid grid-cols-2 gap-4 w-[400px]">
+                    <Skeleton className="rounded-lg">
+                      <div className="h-5 w-full rounded-lg bg-default-200" />
+                    </Skeleton>
+                    <Skeleton className="rounded-lg">
+                      <div className="h-5 w-full rounded-lg bg-default-200" />
+                    </Skeleton>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/app/_containers/supplement_list/container.tsx
+++ src/app/_containers/supplement_list/container.tsx
@@ -1,5 +1,5 @@
 import type { Supplement } from "@/app/_types/types";
-import pRetry from "p-retry";
+import retry from "async-retry";
 import { Suspense } from "react";
 import { SupplementListSkeleton } from "./_components/SupplementListSkelton";
 import { SupplementsPresentation } from "./presentational";
@@ -15,10 +15,10 @@ export default async function SupplementList() {
 }
 
 async function SupplementListContainer() {
-  const response = await pRetry(
-    () => fetch(`${BACKEND_API_URL}/api/supplements`),
+  const response = await retry(
+    async () => await fetch(`${BACKEND_API_URL}/api/supplements`),
     {
-      retries: 5,
+      retries: 4,
       minTimeout: 1000,
       maxTimeout: 16000,
     },
